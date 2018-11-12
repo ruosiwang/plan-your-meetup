ecf62d2e24303cd63b6582496a39a2e63de4367c
Ruosi Wang <ruos925@gmail.com>
2018-11-12 13:51:58 -0500

    update keras wrapper

--- group_classifier.py
+++ group_classifier.py
@@ -2,6 +2,7 @@ import dataset as d
 import helper as h
 import keras_models as k
 
+import pickle
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -13,42 +14,48 @@ MAX_NUM_WORDS = 40000
 SEQ_MAXLEN = 1000
 
 print('HAHA')
-# ------------ DATASET PREPARTION --------------------
-# load data
-descriptions, categories = d.load_group_descriptions()
-# tranform categorical labels(str) to Ids(int) and sorted by popularity
-labels, mapping = d.label_transform(categories)
-# take the top N popoplar categories
-# balance the number of samples in each category
-descriptions, labels = d.prepare_dateset(descriptions, labels,
-                                         category_N=CATEGORY_NUM,
-                                         sample_balance=False)
+# # ------------ DATASET PREPARTION --------------------
+# # load data
+# descriptions, categories = d.load_group_descriptions()
+# # tranform categorical labels(str) to Ids(int) and sorted by popularity
+# labels, mapping = d.label_transform(categories)
+# # take the top N popoplar categories
+# # balance the number of samples in each category
+# descriptions, labels = d.prepare_dateset(descriptions, labels,
+#                                          category_N=CATEGORY_NUM,
+#                                          sample_balance=False)
 
-# ------------ DATA PREPROCESSING --------------------
-# text cleaning
-descriptions = h.clean_texts(descriptions)
-# tokenize group descriptions
-tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
-tokenizer.fit_on_texts(descriptions)
-sequences = tokenizer.texts_to_sequences(descriptions)
-# padd sequences
-sequences = pad_sequences(sequences, maxlen=SEQ_MAXLEN)
+# # ------------ DATA PREPROCESSING --------------------
+# # text cleaning
+# descriptions = h.clean_texts(descriptions)
+# # tokenize group descriptions
+# tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+# tokenizer.fit_on_texts(descriptions)
+# sequences = tokenizer.texts_to_sequences(descriptions)
+# # padd sequences
+# sequences = pad_sequences(sequences, maxlen=SEQ_MAXLEN)
 
-# one hot coding
-onehot_labels = to_categorical(labels)
+# # one hot coding
+# onehot_labels = to_categorical(labels)
 
-# train test split
-X_train, X_test, y_train, y_test = \
-    train_test_split(sequences, onehot_labels, test_size=0.2, random_state=42)
+# # train test split
+# X_train, X_test, y_train, y_test = \
+#     train_test_split(sequences, onehot_labels, test_size=0.2, random_state=42)
 
-# # ----------- WORD EMBEDDING --------------------------
+# # # ----------- WORD EMBEDDING --------------------------
 
-# load glove embedding
-glove = h.load_glove(EMBEDDING_DIM)
-embedding_matrix = h.get_embedding_matrix(tokenizer.word_index, glove,
-                                          MAX_NUM_WORDS, EMBEDDING_DIM)
+# # load glove embedding
+# glove = h.load_glove(EMBEDDING_DIM)
+# embedding_matrix = h.get_embedding_matrix(tokenizer.word_index, glove,
+#                                           MAX_NUM_WORDS, EMBEDDING_DIM)
+
+# pickle.dump([X_train, X_test, y_train, y_test, embedding_matrix],
+#             open("tmp.pkl", "wb"))
+
+[X_train, X_test, y_train, y_test, embedding_matrix] = \
+    pickle.load(open("tmp.pkl", "rb"))
 
 # Conv NN
-cnn = k.build_DNN(embedding_matrix, SEQ_MAXLEN, CATEGORY_NUM, DNN_type="MLP")
+cnn = k.build_DNN(embedding_matrix, SEQ_MAXLEN, CATEGORY_NUM, model_type="CNN_GRU")
 cnn.fit(X_train, y_train, batch_size=128, epochs=5,
         validation_split=0.2)
--- keras_models.py
+++ keras_models.py
@@ -1,11 +1,11 @@
-from keras.layers import Dense, GlobalMaxPooling1D, Dropout, CuDNNGRU
+from keras.layers import Dense, GlobalMaxPooling1D, Dropout, CuDNNGRU, GRU
 from keras.layers import Conv1D, MaxPooling1D, Embedding, Flatten
 from keras.models import Sequential
 from keras.initializers import Constant
 
 
 def build_DNN(embedding_matrix, max_sequence_length,
-              category_num, DNN_type="MLP"):
+              category_num, model_type=None):
     embedding_layer = Embedding(embedding_matrix.shape[0],
                                 embedding_matrix.shape[1],
                                 embeddings_initializer=Constant(embedding_matrix),
@@ -14,10 +14,14 @@ def build_DNN(embedding_matrix, max_sequence_length,
     model = Sequential()
     model.add(embedding_layer)
 
-    if DNN_type == 'MLP':
+    if model_type is 'MLP':
         model = _MLP(model)
-    elif DNN_type == 'CNN':
+    if model_type is 'GRU':
+        model = _GRU(model)
+    if model_type is 'CNN':
         model = _CNN(model)
+    if model_type is "CNN_GRU":
+        model = _CNN_GRU(model)
 
     model.add(Dense(category_num, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
@@ -38,7 +42,7 @@ def _MLP(model, layer_num=2):
 def _CNN(model, layer_num=2):
     for i in range(layer_num):
         model.add(Conv1D(128, 5, activation='relu'))
-        if i == layer_num:
+        if i == layer_num - 1:
             model.add(GlobalMaxPooling1D())
         else:
             model.add(MaxPooling1D(5))
@@ -48,7 +52,20 @@ def _CNN(model, layer_num=2):
 
 def _GRU(model, layer_num=1):
     for _ in range(layer_num):
-        model.add(CuDNNGRU(100))
+        model.add(GRU(100))
+        model.add(Dropout(0.2))
+    return model
+
+
+def _CNN_GRU(model, layer_num=[1, 1]):
+    cnn_lN, gru_lN = layer_num
+    for _ in range(layer_num[0]):
+        model.add(Conv1D(128, 5, activation='relu'))
+        model.add(MaxPooling1D(5))
+        model.add(Dropout(0.2))
+
+    for _ in range(layer_num[1]):
+        model.add(GRU(100))
         model.add(Dropout(0.2))
     return model
 
