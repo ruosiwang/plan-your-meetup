c0b7256afa6d32df58bc3c64a20500766412e8db
Ruosi Wang <ruos925@gmail.com>
2018-11-18 11:09:21 -0500

    use meetup topic instead of category

--- dataset.py
+++ dataset.py
@@ -1,78 +1,83 @@
+import helper as h
+
 import os
 import gzip
 import random
 import pickle
 import ujson as json
-import itertools
-import helper as h
 import numpy as np
-
 from collections import Counter
 from itertools import islice, chain
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 DATA_DIR = 'data'
 GROUP_FILE = 'groups.json.gz'
 EVENT_FILE = 'events.json.gz'
 VENUE_FILE = 'venues.json.gz'
+TOPIC_FILE = 'category_topics.txt'
+CITY_FILE = 'cities.txt'
+
+# use a hidden directory to save intermediate results
+HIDDEN_DIR = os.path.join(DATA_DIR, '.pkl')
+if not os.path.exists(HIDDEN_DIR):
+    os.mkdir(HIDDEN_DIR)
+
+
+# -------------- helper functions ----------------------
+def _load_category_topic_mapping():
+    mapping = {}
+    file = os.path.join(DATA_DIR, TOPIC_FILE)
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            k, v = line.strip().split()
+            mapping[int(k)] = v
+    return mapping
 
 
 def _load_group_descriptions(group_num=None, save_file=None):
     """
-
+    read groups.json.gz to extract meetup group descriptions and categories
     """
-    group_ids = set()  # make sure no replicates
-    descriptions = []  # a list of group descriptions
-    categories = []  # a list of group categories
-
+    category_topic_mapping = _load_category_topic_mapping()
+    descriptions, topics = [], []
     file = os.path.join(DATA_DIR, GROUP_FILE)
     with gzip.open(file) as f:
         for group in (json.loads(x) for x in islice(f, group_num)):
-            dscr = group.get("description")
-            ctg = group.get("category")
-            grpId = group.get("id")
-
-            if all([dscr, ctg, grpId]) and (grpId not in group_ids):
+            dscr = group.get('description')
+            ctg = group.get('category')
+            if dscr and ctg:
                 descriptions.append(dscr)
-                categories.append(ctg.get("shortname"))
-                group_ids.add(grpId)
+                topics.append(category_topic_mapping[ctg['id']])
     # save pickle
-    pickle.dump([descriptions, categories],
+    pickle.dump([descriptions, topics],
                 open(save_file, 'wb'))
-    return descriptions, categories
+    return descriptions, topics
 
 
-def load_group_descriptions(group_num=None):
-    file = os.path.join(DATA_DIR, 'group_dscr')
-    try:
-        return pickle.load(open(file, 'rb'))
-    except FileNotFoundError:
-        return _load_group_descriptions(save_file=file)
-
-
-def _get_group_category_mapping():
-    group_category_mapping = {}
+def _get_group_topic_mapping(save_file):
+    """
+    read group.json.gz to extract id and category
+    """
+    category_topic_mapping = _load_category_topic_mapping()
+    group_topic_mapping = {}
     file = os.path.join(DATA_DIR, GROUP_FILE)
     with gzip.open(file) as f:
         for group in (json.loads(x) for x in f):
-            ctg = group.get("category")
-            if not ctg:
-                grpId = group.get("id")
-                group_category_mapping[grpId] = ctg
-    return group_category_mapping
-
-
-def get_group_category_mapping():
-    file = os.path.join(DATA_DIR, 'group_category_mapping')
-    try:
-        return pickle.load(open(file, 'rb'))
-    except FileNotFoundError:
-        return _get_group_category_mapping()
+            try:
+                group_topic_mapping[group['id']] \
+                    = category_topic_mapping[group['category']['id']]
+            except KeyError:
+                pass
+    # save pickle
+    pickle.dump(group_topic_mapping,
+                open(save_file, 'wb'))
+    return group_topic_mapping
 
 
 def _select_maxlen(examine, descriptions):
-    similarity_items = list(set(itertools.chain(*examine)))
+    similarity_items = list(set(chain(*examine)))
     dscr_len = {i: len(descriptions[i]) for i in similarity_items}
     dscr_maxlen = max(dscr_len, key=dscr_len.get)
     return similarity_items, dscr_maxlen
@@ -114,29 +119,20 @@ def get_unique(descriptions, threshold=0.5):
     return [descriptions[i] for i in unique_ids]
 
 
-def load_event_descriptions(event_num=None):
-    loadpath = os.path.join(DATA_DIR, "event_dscr")
-    try:
-        descriptions, categories = pickle.load(
-            open(loadpath, "rb"))
-    except FileNotFoundError:
-        descriptions, categories = _load_event_descriptions(event_num)
-    return descriptions, categories
-
-
 def _load_event_descriptions(event_num=None):
     """
 
     """
-    descriptions, categories = [], []
+    descriptions, topics = [], []
     group_ids, dscr_tracking = set(), set()
     current_id = None
-    mapping = get_group_category_mapping()
+    mapping = get_group_topic_mapping()
 
     file = os.path.join(DATA_DIR, EVENT_FILE)
     with gzip.open(file) as f:
         for i, event in enumerate((json.loads(x) for x in islice(f, event_num))):
-            print(i)
+            if i % 1000 == 0:
+                print(i)
             dscr = event.get("description")
             if dscr:
                 dscr = h.clean_text(dscr)
@@ -148,10 +144,10 @@ def _load_event_descriptions(event_num=None):
             if current_id and dscr_tracking and (grpId != current_id):
                 unique_descriptions = get_unique(list(dscr_tracking),
                                                  threshold=0.5)
-                category = [mapping[current_id]] * len(unique_descriptions)
+                topic = [mapping[current_id]] * len(unique_descriptions)
 
                 descriptions.extend(unique_descriptions)
-                categories.extend(category)
+                topics.extend(topic)
 
                 group_ids.add(grpId)
                 current_id = grpId
@@ -161,62 +157,82 @@ def _load_event_descriptions(event_num=None):
                 current_id = grpId
             dscr_tracking.add(dscr)
 
-    return descriptions, categories
-
-    # # save pickle
-    # return descriptions, categories
+    return descriptions, topics
 
 
-def load_group(field_list):
-    """
-
-    """
-    group_ids = set()  # make sure no replicates
-    info = []
-
-    file = os.path.join(DATA_DIR, GROUP_FILE)
-    with gzip.open(file) as f:
-        for group in (json.loads(x) for x in f):
-            grpId = group.get('id')
-            this = [group.get(k) for k in field_list]
-
-            if all(this) and (grpId not in group_ids):
-                info.append(this)
-                group_ids.add(grpId)
-
-    # save pickle
-    return info
-
-
-def _get_category_mapping(categories, file_path):
-    counts = Counter(categories)
+def _get_topic_mapping(topics, file_path):
+    counts = Counter(topics)
     ranked = sorted(counts, key=counts.get, reverse=True)
     mapping = {ctg: i for i, ctg in enumerate(ranked)}
     pickle.dump(mapping, open(file_path, 'wb'))
     return mapping
 
 
-def label_transform(categories):
-    file_path = os.path.join(DATA_DIR, 'category_mapping')
+# ------------- main functions -----------------
+def load_group_descriptions(group_num=None):
+    """
+    load group descriptions and categories
+    """
+    file = os.path.join(HIDDEN_DIR, 'group_descriptions')
+    try:
+        return pickle.load(open(file, 'rb'))
+    except FileNotFoundError:
+        return _load_group_descriptions(save_file=file)
+
+
+def get_group_topic_mapping():
+    """
+    load a dictionary of {group_id: category} mapping
+    """
+    file = os.path.join(HIDDEN_DIR, 'group_topic_mapping')
+    try:
+        return pickle.load(open(file, 'rb'))
+    except FileNotFoundError:
+        return _get_group_topic_mapping(file)
+
+
+def load_event_descriptions(event_num=None):
+    """
+    load event descriptions and categories
+    """
+    loadpath = os.path.join(DATA_DIR, "event_dscr")
+    try:
+        descriptions, topics = pickle.load(
+            open(loadpath, "rb"))
+    except FileNotFoundError:
+        descriptions, topics = _load_event_descriptions(event_num)
+    return descriptions, topics
+
+
+def label_transform(topics):
+    """
+    transform topic names to ids using topic_mapping
+    """
+    file_path = os.path.join(HIDDEN_DIR, 'topic_mapping')
     try:
         mapping = pickle.load(open(file_path, 'rb'))
     except FileNotFoundError:
-        mapping = _get_category_mapping(categories, file_path)
-    labels = [mapping[cat] for cat in categories]
+        mapping = _get_topic_mapping(topics, file_path)
+    labels = [mapping[t] for t in topics]
     return labels
 
 
-def prepare_dateset(descriptions, labels, category_num=None, sample_balance=False):
-
-    indicies = {l: [] for l in range(0, category_num)}
+def prepare_dateset(descriptions, labels, topic_ids=[], sample_balance=False):
+    """
+    get pairs of descriptions and labels with requested ids
+    input:
+        topic_ids: e.g. [1:5] (top 5 topics);
+        sample_balance: balance the number of groups in each category
+    """
+    indicies = {l: [] for l in topic_ids}
     for idx, (dscrp, label) in enumerate(zip(descriptions, labels)):
-        if label < category_num:
+        if label in topic_ids:
             indicies[label].append(idx)
 
     selected = []
     if sample_balance:
-        sample_N = labels.count(category_num)
-        for label in range(0, category_num):
+        sample_N = min(map(lambda x: len(x), indicies.values()))
+        for label in topic_ids:
             selected.extend(random.sample(indicies[label], sample_N))
     else:
         selected = list(chain(*list(indicies.values())))
---
+++ topic_classifier.py
@@ -0,0 +1,105 @@
+import os
+import pickle
+import dataset as d
+import helper as h
+import keras_models as k
+import sklearn_models as s
+
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+
+DATA_TYPE = 'group'
+MODEL_GROUP = 'sklearn'  # or sklearn
+TOPIC_NUM = [5, 10, 15, 20]
+EMBEDDING_DIM = 50
+MAX_NUM_WORDS = 40000
+SEQ_MAXLEN = 1000
+
+
+Keras_models = [('MLP', 2), ('GRU', 1), ('CNN', 3), ('CNN_GRU', [2, 1])]
+sklearn_models = ['MNB', 'SVC', 'ETC', 'RFC']
+
+
+# load data and prepare
+def make_dataset(data_type='group', topic_num=None, sample_balance=False):
+    print(f"prepare {data_type} dataset...")
+    if data_type is 'group':
+        descriptions, topics = d.load_group_descriptions()
+    elif data_type is 'event':
+        descriptions, topics = d.load_event_descriptions()
+
+    # tranform categorical labels(str) to Ids(int) and sorted by popularity
+    labels = d.label_transform(topics)
+
+    # take the top N popoplar topics
+    # balance the number of samples in each category
+    descriptions, labels = d.prepare_dateset(descriptions, labels,
+                                             topic_ids=range(topic_num),
+                                             sample_balance=False)
+    # text cleaning
+    descriptions = h.clean_texts(descriptions)
+
+    # dataset descriptions:
+    print(f'This dataset includes {len(descriptions)} {data_type}s.')
+    return descriptions, labels
+
+
+# DATA PREPROCESSING FOR DEEP LEARNING
+def data_transform(descriptions, labels, max_num_words, seq_maxlen):
+    print("data preprocessing...")
+    # tokenize group descriptions
+    tokenizer = Tokenizer(num_words=max_num_words)
+    tokenizer.fit_on_texts(descriptions)
+    sequences = tokenizer.texts_to_sequences(descriptions)
+    # padd sequences
+    sequences = pad_sequences(sequences, maxlen=seq_maxlen)
+    # one hot coding
+    onehot_labels = to_categorical(labels)
+    print("preprocessing done...")
+    return sequences, onehot_labels, tokenizer.word_index
+
+
+if __name__ == '__main__':
+    for num in TOPIC_NUM:
+        descriptions, labels = make_dataset(data_type=DATA_TYPE,
+                                            topic_num=num,
+                                            sample_balance=False)
+
+        if MODEL_GROUP is 'keras':
+            sequences, onehot_labels, word_index = data_transform(descriptions, labels,
+                                                                  MAX_NUM_WORDS, SEQ_MAXLEN)
+            # load glove embedding
+            glove = h.load_glove(EMBEDDING_DIM)
+            embedding_matrix = h.get_embedding_matrix(word_index, glove,
+                                                      MAX_NUM_WORDS, EMBEDDING_DIM)
+
+        # # train test split
+        X_train, X_val, y_train, y_val = \
+            train_test_split(descriptions, labels, test_size=0.2, random_state=42)
+        # save
+        validation_file_name = f'validation_split_{num:02d}_topics'
+        validation_path = os.path.join(d.DATA_DIR, validation_file_name)
+        pickle.dump([X_val, y_val], open(validation_path, 'wb'))
+
+        # if MODEL_GROUP is 'keras':
+        #     for (model_type, layer_num) in Keras_models:
+        #         print(f'{model_type} training')
+        #         model_name = f'{model_type}_{num:02d}_{DATA_TYPE}'
+        #         model = k.build_DNN(embedding_matrix, SEQ_MAXLEN,
+        #                             num, model_type=model_type)
+        #         callbacks = k.set_callback(model_name)
+        #         model.fit(X_train, y_train, epochs=10,
+        #                   batch_size=128, callbacks=callbacks,
+        #                   validation_split=0.2)
+
+        # if MODEL_GROUP is 'sklearn':
+        #     for model_type in sklearn_models:
+        #         print(f'{model_type} training')
+        #         model_name = f'{model_type}_{num:02d}_{DATA_TYPE}'
+        #         model = s.build_SKM(model_type=model_type,
+        #                             max_features=MAX_NUM_WORDS,
+        #                             selectK=10000)
+        #         model.fit(X_train, y_train)
+        #         s.save_model(model, model_name)
